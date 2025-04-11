b42897db653db4d602e27d359348b2f78a40ab41
Nate-Mei <n.mei33@yahoo.com>
2025-04-10 23:00:58 -0700

    finished portfolio

--- nathan/Portrait__u_.js
+++ nathan/Portrait__u_.js
@@ -4,23 +4,43 @@ function setup() {
 }
 function draw() {
      strokeWeight(0);
+  // --- Hair (Back Layer) ---
+  for (let i = 0; i < 10; i++) {
+    fill(0, 0, 0); // dark to light fade
+    ellipse(300, 300, 330 - i * 5, 440 - i * 4); // layered ellipses
+  }
+  
   //head
-  fill(242, 214, 181) // Skin Color
+  fill(224,182,138) // Skin Color
   ellipse(300, 300, 300, 425); // Position
 
   //left eye
   fill(250) // Eye Color
-  ellipse(230, 200, 60, 50); // Position
+  ellipse(225, 250, 60, 50); // Position
   
   //left pupil
   fill(84,42,14) // Pupil color
-  ellipse(230, 200, 30, 30); // Position
+  ellipse(225, 250, 30, 30); // Position
   
   //right eye
   fill(250) // Eye Color
-  ellipse(360, 200, 60, 50); // Position
+  ellipse(365, 250, 60, 50); // Position
   
   //right pupil
   fill(84,42,14) // Pupil color
-  ellipse(360, 200, 30, 30); // Position
+  ellipse(365, 250, 30, 30); // Position
+  
+  // mouth
+  stroke(84, 42, 14); // mouth color
+  strokeWeight(5);    // thickness of the mouth line
+  line(240, 370, 360, 370); // from left to right
+  
+  // --- Top Hair Arc ---
+  noStroke();
+  fill(0, 0, 0); // hair color
+  arc(300, 220, 300, 300, PI, TWO_PI); // position, size, start angle, end angle
+  
+  // Shirt
+  fill(50, 50, 50);
+  rect(210, 450, 180, 250, 50);
 }
